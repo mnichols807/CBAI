f7e048abf1537727a57aea04eb55a35d4058012c
mnichols807 <mnichols807@gmail.com>
2021-10-19 13:04:30 +0000

    more testing

--- main.py
+++ main.py
@@ -13,29 +13,6 @@ cls()
 #after this run runtest() to use the program
 runtest()
 """
-from flask import *
-#importing all the methods, classes, functions from flask
-
-
-app = Flask(__name__)
-
-
-#This is the first page that comes when you type localhost:5000... it will have a a tag that redirects to a page
-@app.route("/CBAI")
-def  HomePage():
-    return
-
-#Once it redirects here (to localhost:5000/runscript) it will run the code before the return statement
-@app.route("/CBAI")
-def ScriptPage():
-    print("<html><body>TestingTesting</body></html>")
-    #Type what you want to do when the user clicks on the link
-    # once it is done with doing that code... it will redirect back to the homepage
-    return redirect(url_for("HomePage"))
-
-#Running it only if we are running it directly from the file... not by importing
-if __name__ == "__main__":
-    app.run(debug=True)
 import os
 import importlib
 import sys
